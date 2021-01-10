f2071c092a4279693589623fc72ba7aaaf3bf596
MHendriF <hendrifebriansyah28@gmail.com>
2021-01-10 14:36:27 +0700

    remove unused code

--- libraries/common/src/main/java/com/mhendrif/capstone/common/util/Constants.kt
+++ libraries/common/src/main/java/com/mhendrif/capstone/common/util/Constants.kt
@@ -4,8 +4,7 @@ enum class SortOrder { BY_NAME, BY_DATE }
 
 object Constants {
     const val TIME_OUT: Long = 120
-    const val DELAY: Long = 3000
-    const val PREFERENCE_NAME = "user_preference"
+    const val DELAY: Long = 2500
     const val DATABASE_NAME = "movie_catalogue_local.db"
     const val MOVIE_TABLE_NAME = "movies"
     const val TV_SHOW_TABLE_NAME = "tvShows"
@@ -15,5 +14,4 @@ object Constants {
     const val TMDB_TV_URL = "https://www.themoviedb.org/tv/"
     const val API_POSTER_PATH = "https://image.tmdb.org/t/p/w185"
     const val API_BACKDROP_PATH = "https://image.tmdb.org/t/p/w500"
-    const val SORT_ORDER = "sort_order"
 }
