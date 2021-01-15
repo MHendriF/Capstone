7cb699d58fa9a57ebb8a06dd62e03f321f2d1581
MHendriF <hendrifebriansyah28@gmail.com>
2021-01-15 17:16:38 +0700

    update config detekt

---
+++ app/src/androidTest/java/com/mhendrif/capstone/SplashActivityTest.kt
@@ -0,0 +1,13 @@
+package com.mhendrif.capstone
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
+
+class SplashActivityTest {
+    @get:Rule
+    var activity = ActivityScenarioRule(SplashActivity::class.java)
+
+    @Test
+    fun loadSplash() {}
+}
