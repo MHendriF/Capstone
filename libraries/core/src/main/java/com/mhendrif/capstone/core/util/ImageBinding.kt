59d7fe833ca880fb2c43ee65551bd6438a770f6e
MHendriF <hendrifebriansyah28@gmail.com>
2021-01-16 09:34:45 +0700

    fix warning code styling

--- libraries/core/src/main/java/com/mhendrif/capstone/core/util/ImageBinding.kt
+++ libraries/core/src/main/java/com/mhendrif/capstone/core/util/ImageBinding.kt
@@ -4,22 +4,23 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
+import timber.log.Timber
 
 object ImageBinding {
     @BindingAdapter("android:imageURL")
     @JvmStatic
-    fun setImageURL(imageView: ImageView, URL: String?) {
+    fun setImageURL(imageView: ImageView, url: String?) {
         try {
             imageView.alpha = 0f
-            Picasso.get().load(URL).noFade().into(
+            Picasso.get().load(url).noFade().into(
                 imageView,
                 object : Callback {
                     override fun onSuccess() {
                         imageView.animate().setDuration(300).alpha(1f).start()
                     }
-                    override fun onError(e: Exception?) {}
+                    override fun onError(e: Exception?) { Timber.e(e) }
                 }
             )
-        } catch (ignored: Exception) { }
+        } catch (e: Exception) { Timber.e(e) }
     }
 }
