5ef02b6e79ad411e41232690d9785746bc28773b
MHendriF <hendrifebriansyah28@gmail.com>
2021-01-19 19:18:59 +0700

    Add unit testing

---
+++ features/favorite/src/test/java/com/mhendrif/capstone/favorite/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.mhendrif.capstone.favorite
+
+import org.junit.Assert
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
